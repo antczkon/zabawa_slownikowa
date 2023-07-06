a5adfdd98def74072c798edd0e7c16775ee9bfa7
antczkon <135462573+antczkon@users.noreply.github.com>
2023-07-06 09:56:01 +0200

    Add files via upload
    added TDD

--- Project_test/main.py
+++ Project_test/main.py
@@ -6,8 +6,37 @@ def unpacking(filename):
     weekday_names=[]
 
     for row in f:
+        row = row.split()
         month,day_num,weekday_name=row[0], row[1], row[2]
         months.append(month)
         days_num.append(day_num)
         weekday_names.append(weekday_name)
-    return 'a'
+    return months, days_num, weekday_names
+
+
+def num_of_days_in_months(months_list):
+    dict_of_days_in_months={}
+    for month in months_list:
+        if month not in dict_of_days_in_months.keys():
+            dict_of_days_in_months[month] = 1
+        else:
+            dict_of_days_in_months[month] +=1
+    return dict_of_days_in_months
+
+
+def dict_of_dicts_month_number_weekday(months_list, days_num_list, weekdays_list):
+    dict_of_days_in_months={}
+    for month in months_list:
+        if month not in dict_of_days_in_months.keys():
+            dict_of_days_in_months[month] = {}
+    for i in range(len(days_num_list)):
+        dict_of_days_in_months[months_list[i]][days_num_list[i]] = weekdays_list[i]
+    return dict_of_days_in_months
+
+def dict_of_dicts_month_weekday_its_numbers(dictionar):
+    new_dict ={}
+    for month in dictionar.keys():
+        new_dict[month] = {}
+        for val in dictionar[month].values():
+            new_dict[month][val] = [key for key in dictionar[month].keys() if dictionar[month][key]==val]
+    return new_dict
